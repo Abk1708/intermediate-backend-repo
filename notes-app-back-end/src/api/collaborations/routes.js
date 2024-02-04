a7fed80530cd134f0729746955036aaf13d118ae
Abk1708 <bgus1708@gmail.com>
2024-02-04 21:08:51 +0700

    collaboration implemented

---
+++ notes-app-back-end/src/api/collaborations/routes.js
@@ -0,0 +1,20 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/collaborations',
+    handler: handler.postCollaborationHandler,
+    options: {
+      auth: 'internotesapp_jwt',
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/collaborations',
+    handler: handler.deleteCollaborationHandler,
+    options: {
+      auth: 'internotesapp_jwt',
+    },
+  },
+];
+
+module.exports = routes;
